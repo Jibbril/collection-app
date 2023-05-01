74f00416e77f16ed0b50ac95fef2b7ee57fb8b98
Jibbril Berbres <jibbril.berbres@viego.se>
2023-05-01 22:30:59 +0200

    Add shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Mail } from "lucide-react";
+import { Button } from "@/components/shadcn-ui/button";
+
 export default function HomePage() {
-  return <h1>Root page!</h1>;
+  return (
+    <Button>
+      <Mail className="mr-2 h-4 w-4" /> Login with Email
+    </Button>
+  );
 }
