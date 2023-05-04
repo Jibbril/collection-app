9cc9503f25c531e492f0deaed04f3a7f5332719c
Jibbril Berbres <jibbril.berbres@viego.se>
2023-05-04 23:48:09 +0200

    Setup prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,12 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  jsxSingleQuote: true,
+  singleQuote: true,
 };
 
 module.exports = config;
