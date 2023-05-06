4db04950469d1646bec166cf85c22f7af5782514
Jibbril Berbres <jibbril.berbres@viego.se>
2023-05-06 11:59:17 +0200

    Setup basic Clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,31 @@
-import "../styles/globals.css";
-import { Inter } from "next/font/google";
-import Navbar from "@/components/navigation/Navbar";
+import '../styles/globals.css';
+import { Inter } from 'next/font/google';
+import Navbar from '@/components/navigation/Navbar';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
+  subsets: ['latin'],
+  display: 'swap',
 });
 
 export default function RootLayout({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={inter.className}>
-      <body>
-        <Navbar />
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang='en' className={inter.className}>
+        <body>
+          <Navbar />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
 
 export const metadata = {
-  title: "Collection app",
-  description: "Collect anything, remember everything.",
+  title: 'Collection app',
+  description: 'Collect anything, remember everything.',
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import { Mail } from "lucide-react";
-import { Button } from "@/components/shadcn-ui/button";
+import { Mail } from 'lucide-react';
+import { Button } from '@/components/shadcn-ui/button';
+import { UserButton } from '@clerk/nextjs';
 
 export default function HomePage() {
   return (
-    <Button>
-      <Mail className="mr-2 h-4 w-4" /> Login with Email
-    </Button>
+    <>
+      <Button>
+        <Mail className='mr-2 h-4 w-4' /> Login with Email
+      </Button>
+      <div>
+        <UserButton />
+      </div>
+    </>
   );
 }
